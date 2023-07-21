272577c1a17d85936af4d7d25fe43fb91ed90f01
xxudon <ahlinder.leo@icloud.com>
2023-07-21 20:24:35 +0200

    test for admins

--- test.js
+++ test.js
@@ -1,40 +1,50 @@
-const supertest = require('supertest');
-const chai = require('chai');
-const server = require('./server'); // Adjust the path to your server.js file
+// Import necessary modules
+const app = require('./server'); // Replace this with the path to your Express app
+const request = require('supertest');
+const { expect } = require('chai');
 
-const request = supertest.agent(server);
-const expect = chai.expect;
+// Describe the test suite
+describe('Admin Page API', () => {
+  // Test the "/api/adminpage" POST endpoint
+  describe('POST /api/adminpage', () => {
+    it('should return "Admin exist" if the admin exists in the database', (done) => {
+      // Mock request body with an existing admin
+      const requestBody = { namn: 'leo', kod: '1111' };
 
-describe('POST /api/adminpage', () => {
-  it('should respond with "Admin exist" when valid admin credentials are provided', (done) => {
-    const admin = {
-      namn: 'validAdminName',
-      kod: 'validAdminPassword'
-    };
+      // Send the request to the app
+      request(app)
+        .post('/api/adminpage')
+        .send(requestBody)
+        .expect(200) // Expect HTTP status code 200
+        .end((err, res) => {
+          if (err) return done(err);
 
-    request.post('/api/adminpage')
-      .send(admin)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.message).to.equal('Admin exist');
-      })
-      .end(done);
+          // Make assertions on the response
+          expect(res.body.message).to.equal('Admin exist');
+
+          done(); // Callback to indicate the test is complete
+        });
+    });
+
+    it('should return "Admin does not exist" if the admin does not exist in the database', (done) => {
+      // Mock request body with a non-existing admin
+      const requestBody = { namn: 'NonExistingAdminName', kod: 'AdminCode' };
+
+      // Send the request to the app
+      request(app)
+        .post('/api/adminpage')
+        .send(requestBody)
+        .expect(200) // Expect HTTP status code 200
+        .end((err, res) => {
+          if (err) return done(err);
+
+          // Make assertions on the response
+          expect(res.body.message).to.equal('Admin does not exist');
+
+          done(); // Callback to indicate the test is complete
+        });
+    });
+
+    // Add more test cases if needed for other scenarios (e.g., error cases)
   });
-
-  it('should respond with "Admin does not exist" when invalid admin credentials are provided', (done) => {
-    const admin = {
-      namn: 'invalidAdminName',
-      kod: 'invalidAdminPassword'
-    };
-
-    request.post('/api/adminpage')
-      .send(admin)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.message).to.equal('Admin does not exist');
-      })
-      .end(done);
-  });
-
-  // Add more test cases for other scenarios if needed
 });
